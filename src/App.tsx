bfc1219e78240b43c7a92e5f41b02b85043c3a89
Ryan Nograles <125443916+ryan-nograles@users.noreply.github.com>
2024-04-18 07:55:45 +0800

    change sample icon front to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import reactLogo from "../public/pwa-512x512.png";
 import "./App.css";
 
 function App() {
@@ -10,13 +9,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>SMPW Information Management System (PWA)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
